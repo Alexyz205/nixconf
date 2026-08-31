8486d387fba52d65b28cb5349516a41b5e715364
Alexyz205 <anathos205@gmail.com>
2026-08-31 23:52:19 +0200

    fix podman network issue

--- modules/system/podman.nix
+++ modules/system/podman.nix
@@ -3,6 +3,8 @@
     boot.kernelModules = [
       "overlay"
       "fuse"
+      # Rootless pasta/slirp4netns networking needs /dev/net/tun.
+      "tun"
     ];
     virtualisation.podman = {
       enable = true;
